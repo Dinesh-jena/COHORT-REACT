7da1582c9f072a7f998ca1993da990691115eea1
DINESH JENA <dineshakhay736@gmail.com>
2026-06-10 10:01:30 +0530

    Formik

--- Day72/src/App.jsx
+++ Day72/src/App.jsx
@@ -1,47 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [Dinesh, setDinesh] = useState([
+    { name: "Dinesh Jena", age: 20 },
+    { name: "Dinesh Jena", age: 20 },
+    { name: "Dinesh Jena", age: 20 },
+  ]);
 
-  const [Dinesh,setDinesh] = useState(  [
-    { name:"Dinesh Jena",age:20 },
-     { name:"Dinesh Jena",age:20 },
-      { name:"Dinesh Jena",age:20 },
-  ])
-  
-
-
-  function SubmitHandler(){
+  function SubmitHandler() {
     let copytodo = [...Dinesh];
     copytodo.pop();
     setDinesh(copytodo);
   }
 
-
-
-  
   return (
     <div>
-      
-        {Dinesh.map((item ,index) => {
-                return (
+      {Dinesh.map((item, index) => {
+        return (
           <div key={index}>
             <h2>{item.name}</h2>
             <p>{item.age}</p>
           </div>
-        )
+        );
+      })}
 
-        })}
-
-
-        <button onClick={SubmitHandler}>Delete</button>
-        <br />
-        <br /><br />
-        <input type="text"  />
-        <br /><br />
-        <button>Add Todo</button>
-        
+      <button onClick={SubmitHandler}>Delete</button>
+      <br />
+      <br />
+      <br />
+      <input type="text" />
+      <br />
+      <br />
+      <button>Add Todo</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Day78/src/components/Create.jsx
+++ Day78/src/components/Create.jsx
@@ -1,55 +1,55 @@
-import React from 'react'
-import {useState} from 'react'
-import { nanoid } from 'nanoid';
+import React from "react";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 const Create = (props) => {
+  const todos = props.todos;
+  const setTodos = props.setTodos;
 
-    const todos = props.todos;
-    const setTodos = props.setTodos;
+  const [title, settitle] = useState("");
+  // const[Completed, setcompleted] = useState(false);
 
-    const[title,settitle]=useState("");
-    // const[Completed, setcompleted] = useState(false);
+  const SubmitHandler = (e) => {
+    e.preventDefault(); //page refresh hone se bachaya
 
-     const SubmitHandler =(e) =>{
-                    e.preventDefault();//page refresh hone se bachaya 
-    
-                    const newtodo ={
-                      id: nanoid(),
-                      title : title,
-                      isCompleted: false,
-                    }
-    
-    
-                    //1.option
-                    let copytodos = [...todos];//array ko copy kiya 
-                    copytodos.push(newtodo);//array mein add kiya 
-                    setTodos(copytodos);//abb pure array ko wapas se addd kar diya 
-                    
-                    //2.option
-                    // setTodos([...todos,newtodo])
-    
-                    settitle("");
-                  }
+    const newtodo = {
+      id: nanoid(),
+      title: title,
+      isCompleted: false,
+    };
 
+    //1.option
+    let copytodos = [...todos]; //array ko copy kiya
+    copytodos.push(newtodo); //array mein add kiya
+    setTodos(copytodos); //abb pure array ko wapas se addd kar diya
+
+    //2.option
+    // setTodos([...todos,newtodo])
+
+    settitle("");
+  };
 
   return (
-    <div className='border w-[70%] p-10 text-center'>   
-            <h1 className="mb-10  text-4xl text-thin ">Set <span className='text-red-300'>Reminder</span> For Task</h1>
-            <br />
-            <form onSubmit={SubmitHandler} >
-                    <input
-                    onChange={(e)=>settitle(e.target.value)}//jisme valude on usmee.target.value
-                    value={title}
-                    className="border-b w-full text-2xl font-thin p-2 outline-0 "
-                    type="text" 
-                     placeholder='title'/>
-                    <br />
-                    {/* <input 
+    <div className="border w-[70%] p-10 text-center">
+      <h1 className="mb-10  text-4xl text-thin ">
+        Set <span className="text-red-300">Reminder</span> For Task
+      </h1>
+      <br />
+      <form onSubmit={SubmitHandler}>
+        <input
+          onChange={(e) => settitle(e.target.value)} //jisme valude on usmee.target.value
+          value={title}
+          className="border-b w-full text-2xl font-thin p-2 outline-0 "
+          type="text"
+          placeholder="title"
+        />
+        <br />
+        {/* <input 
                     onChange={(e)=>setcompleted(e.target.checked)} 
                     value={Completed}
                     type="checkbox"  />Completed */}
-                    <br />
-                                                            {/* <input 
+        <br />
+        {/* <input 
                                                             value="male"
                                                             onChange={(e)=>setgender(e.target.value)}
                                                             checked ={gender == "male" }
@@ -64,8 +64,8 @@ const Create = (props) => {
                                                             type="radio" 
                                                             />
                                                             female */}
-                                                            <br />
-                                                            {/* <select 
+        <br />
+        {/* <select 
                                                             onChange={(e) => setcity(e.target.value)}
                                                             value={city}
                                                             className='bg-white border-5rem text-black border-5rem'>
@@ -74,11 +74,14 @@ const Create = (props) => {
                                                             <option value="Surat">Surat</option>
                                                             </select> */}
 
-                    <button className="text-xl px-10 py-2 border rounded">Create todos</button>
-                    <br /><br />
-            </form>
+        <button className="text-xl px-10 py-2 border rounded">
+          Create todos
+        </button>
+        <br />
+        <br />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
